2c2ac0960c3732f6afdb77d0c596994c7bad4f46
Artem Vasil <artemiy.paolo@gmail.com>
2024-06-10 13:37:49 +0200

    minor improvement of events-service

--- src/events-service.ts
+++ src/events-service.ts
@@ -113,6 +113,18 @@ class EventsService {
         }
     }
 
+    getTime() {
+        const now = new Date();
+
+        const year = now.getUTCFullYear();
+        const month = (now.getUTCMonth() + 1).toString().padStart(2, '0'); // getUTCMonth() возвращает месяц от 0 до 11
+        const day = now.getUTCDate().toString().padStart(2, '0');
+        const hours = now.getUTCHours().toString().padStart(2, '0');
+        const minutes = now.getUTCMinutes().toString().padStart(2, '0');
+        const seconds = now.getUTCSeconds().toString().padStart(2, '0');
+
+        return `${year}.${month}.${day} ${hours}:${minutes}:${seconds} UTC`;
+    }
 }
 
 export default new EventsService();
